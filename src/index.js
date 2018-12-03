fec7987f5204d60e664ce7c263ca738a2edf2a73
nlznakub <zinachen1@gmail.com>
2018-12-03 16:43:32 +0700

    typeorm

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { createConnection } from 'typeorm';
+import User from './entity/User';
+createConnection().then(async (connection) => {
+    const user = new User();
+    user.name = 'John';
+
+    await connection.manager.save(user)
+})
