cd5dbc649bb7ce008888c205d71ce9d7c7c7ab58
nlznakub <zinachen1@gmail.com>
2018-12-03 17:10:49 +0700

    typeorm

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+var _entity = require('./entity');
+
+var _entity2 = _interopRequireDefault(_entity);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+(0, _entity2.default)();
