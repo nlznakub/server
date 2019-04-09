64ca2bfeb70e117debbc0b8ab050671a96d494f6
zinadev <zinadeveloper@gmail.com>
2019-04-09 16:24:57 +0700

    change host

--- src/knexfile.js
+++ src/knexfile.js
@@ -2,6 +2,7 @@ module.exports = {
   development: {
     client: 'mysql', // type database
     connection: {
+      host: 'ls-c5b066fd8faa374749251d41a936472d4ddbcad4.c9sslnzrd2wa.ap-southeast-1.rds.amazonaws.com',
       user: 'dbmasteruser', // username
       password: 'peer1234', // password
       database: 'tutorial' // database name
